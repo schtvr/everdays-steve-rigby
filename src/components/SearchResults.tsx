72fccd1451fb26e1c8afe09b9b611db2b321df68
steve rigby <stevenrigby.me@gmail.com>
2021-12-12 15:36:39 -0500

    results now fetch new data on change of search state

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import HomeResults from './HomeResults';
 import Pagination from './Pagination';
+import { useSelector } from 'react-redux'
+import { useAppSelector } from '../app/hooks';
 
 const Results = () => {
   const [error, setError] = useState(false);
   const [homes, setHomes] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false);
+  const search = useAppSelector((state) => state.search)
 
   useEffect(() => {
     fetch('http://dev.everdays.com/v2/public/providers?state=mi')
@@ -22,6 +25,24 @@ const Results = () => {
           console.log({error})
         })
   }, [])
+
+  useEffect(() => {
+    const { sort, page } = search
+    fetch(`http://dev.everdays.com/v2/public/providers?state=mi&sort=${sort}&page=${page}`)
+      .then(res => res.json())
+      .then(
+        ({ data }) => {
+          setIsLoaded(true);
+          setHomes(data.homes);
+      },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+          console.log({error})
+        })
+  }, [search])
+
+
   if (error) return <div>Error</div>;
   if (!isLoaded) return <div>Loading...</div>
   if (homes.length < 1) return <div>Empty list</div>
