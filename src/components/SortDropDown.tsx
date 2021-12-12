0c0b8c357eaf08183c5783d1d19d9823773064bc
steve rigby <stevenrigby.me@gmail.com>
2021-12-12 15:57:29 -0500

    added bootstrap for styling and resp design

--- src/components/SortDropDown.tsx
+++ src/components/SortDropDown.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const SortDropDown = () => {
   return (
     <div className="dropdown">
-  <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
+  <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
     Dropdown button
   </button>
-  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a className="dropdown-item" href="search&sort=R">Top Rated</a>
-    <a className="dropdown-item" href="search&sort=A">A-Z</a>
-    <a className="dropdown-item" href="search&sort=Z">Z-A</a>
-  </div>
+  <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a className="dropdown-item" href="#">Action</a></li>
+    <li><a className="dropdown-item" href="#">Another action</a></li>
+    <li><a className="dropdown-item" href="#">Something else here</a></li>
+  </ul>
 </div>
-  )
-}
 
-export default SortDropDown
+    //     <div className="dropdown">
+    //   <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
+    //     Dropdown button
+    //   </button>
+    //   <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    //     <a className="dropdown-item" href="search&sort=R">Top Rated</a>
+    //     <a className="dropdown-item" href="search&sort=A">A-Z</a>
+    //     <a className="dropdown-item" href="search&sort=Z">Z-A</a>
+    //   </div>
+    // </div>
+  );
+};
+
+export default SortDropDown;
