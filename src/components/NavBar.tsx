b74f512cbe69cfcb93f7f96a37be3cf94006f2f6
steve rigby <stevenrigby.me@gmail.com>
2021-12-13 07:50:07 -0500

    added comments to navbar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 const NavBar = () => {
+  // some combo of useState and useEffect to track the scrollY value
+  // when scrollY < 15, add 'bg-transparent'
 
   return (
     <nav className="navbar navbar-expand-md bg-light fixed-top">
